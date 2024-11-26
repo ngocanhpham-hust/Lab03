1b5232865c5aac5e50d3ba01452ef3c130c5608f
ngocanhpham-hust <Ngocanhphamthi05@gmail.com>
2024-11-26 22:58:14 +0700

    Delete

--- OtherProjects/src/hust/soict/dsai/lab01/Section221.java
+++
@@ -1,8 +0,0 @@
-package hust.soict.dsai.lab01;
-public class Section221{
-    public static void main(String[] args){
-        System.out.println("Xin chao \n cac ban");
-        System.out.println("Hello \t World!");
-    }
-
-}
--- OtherProjects/src/hust/soict/dsai/lab01/Section222.java
+++
@@ -1,7 +0,0 @@
-package hust.soict.dsai.lab01;
-import javax.swing.JOptionPane;
-public class Section222{
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello World! How are ya?");
-    }
-}
